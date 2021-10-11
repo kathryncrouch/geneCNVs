b9e5d32d98722484d9ac03c23260f444bd1255e9
Kathryn <crouch.k@gmail.com>
2021-10-11 17:12:36 +0100

    Type for ploidy argument must be int

--- calculateCNVs.py
+++ calculateCNVs.py
@@ -12,7 +12,7 @@ def __main__():
     parser.add_argument('--input', required=True, help='Read counts per gene or transcript in htseq-count format' )
     parser.add_argument('--orthologFile', help='File containing gene to ortholog mappings' )
     parser.add_argument('--outputPrefix', required=True, help='Prefix for all output files' )
-    parser.add_argument('--ploidy', default=2, help='Canonical ploidy for this organism')
+    parser.add_argument('--ploidy', default=2, type=int, help='Canonical ploidy for this organism')
     args = parser.parse_args()
 
     # hack to use TPM module as this is for RNAseq and expects a stranded arg
